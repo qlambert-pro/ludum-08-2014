b8100637ec97951e3972505640af226166435c18
Thibaud Lambert <Thibaud.Lambert@gmail.com>
2014-08-23 15:09:23 +0200

    Update player

--- core/src/com/ludum/configuration/ConfigManager.java
+++ core/src/com/ludum/configuration/ConfigManager.java
@@ -4,6 +4,10 @@ public class ConfigManager {
 	public static int minBlockSize = 16;
 	
 	public static float gravity = 120;
+	public static float playerSizeX = 64;
+	public static float playerSizeY = 64;
+	public static float playerPhysSizeX = 32;
+	public static float playerPhysSizeY = 64;
 	
 	public static float physicsStepSize = 1f/60f;
 }
--- core/src/com/ludum/entity/player/Player.java
+++ core/src/com/ludum/entity/player/Player.java
@@ -1,6 +1,8 @@
 package com.ludum.entity.player;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.ludum.configuration.ConfigManager;
 import com.ludum.entity.Drawable;
 import com.ludum.skill.Skill;
 import com.ludum.entity.Entity;
@@ -9,11 +11,33 @@ public class Player extends Entity implements Drawable{
 	
 	private Skill s1;
 	private Skill s2;
+
+	private Vector2 position;
+	private Vector2 size;
+	private Vector2 physicsSize;
+	
+	private Vector2 acc;
 	
 	
 	public Player(Skill s1, Skill s2){
 		this.s1 = s1;
 		this.s2 = s2;
+		
+		acc = new Vector2(0,0);
+		size = new Vector2(ConfigManager.playerSizeX,ConfigManager.playerSizeY);
+		physicsSize = new Vector2(ConfigManager.playerPhysSizeX,ConfigManager.playerPhysSizeY);
+	}
+	
+	public void moveRight() {
+		
+	}
+	
+	public void moveLeft() {
+		
+	}
+	
+	public void jump() {
+		
 	}
 	
 	public void useSkill1(){
