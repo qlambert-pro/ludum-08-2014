cd36de2b3e253e20f5a12cbddc7970d4ff8327dd
Jeremy Morel <jeremy.morel.jm@gmail.com>
2014-08-23 19:12:01 +0200

    add name search in map + list of Rectangle colli

--- core/src/com/ludum/map/Map.java
+++ core/src/com/ludum/map/Map.java
@@ -3,14 +3,22 @@ package com.ludum.map;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 
 
 
@@ -31,7 +39,8 @@ public class Map extends ApplicationAdapter {
 		camera.setToOrtho(false,w,h);
 		camera.update();
 		tiledMap = new TmxMapLoader().load("stupidMap.tmx");
-		tiledMap.getLayers().get(3).setVisible(false);
+		changeWorld();
+		ArrayList<Rectangle> test=  getCollisionNormalWorld();
 		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
 	}
@@ -45,14 +54,59 @@ public class Map extends ApplicationAdapter {
 		tiledMapRenderer.setView(camera);
 		tiledMapRenderer.render();
 	}
+
+
 	public void changeWorld(){
-		if(tiledMap.getLayers().get(3).isVisible()){
-			tiledMap.getLayers().get(3).setVisible(false);
+		String layerDark="foregroundDarK";
+		String layerWhite="foregroundWhite";
+		if(getLayers(layerDark).isVisible()){
+			getLayers(layerDark).setVisible(false);
+			getLayers(layerWhite).setVisible(true);
 		}else{
-			tiledMap.getLayers().get(3).setVisible(true);
+			getLayers(layerDark).setVisible(true);
+			getLayers(layerWhite).setVisible(false);
 
 		}
 	}
+
+	private MapLayer getLayers(String layerName){
+		return tiledMap.getLayers().get(layerName);
+
+	}
+
+	private ArrayList<Rectangle> getCollision(String layerName){
+		MapLayer collision = getLayers(layerName);
+		MapLayer collisionNormal = getLayers("coli");
+		ArrayList<Rectangle> rectangleList=new ArrayList<Rectangle>();
+
+		for (int i=0;i<collision.getObjects().getCount();i++){
+
+			MapObjects MapObjects=collision.getObjects();
+			rectangleList.add(((RectangleMapObject)MapObjects.get(i)).getRectangle());
+		}
+
+		for (int j=0;j<collisionNormal.getObjects().getCount();j++){
+
+			MapObjects MapObjects=collision.getObjects();
+			rectangleList.add(((RectangleMapObject)MapObjects.get(j)).getRectangle());
+		}
+
+		return rectangleList;
+
+	}
+
+	public ArrayList<Rectangle> getCollisionNormalWorld(){
+		return getCollision("coliNormal");
+	}
+
+
+
+	public ArrayList<Rectangle> getCollisionDarkWorld(){
+		return getCollision("coliDark");
+
+	}
+
+
 
 }
 
