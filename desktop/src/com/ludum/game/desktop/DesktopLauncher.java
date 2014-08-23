7d8a33479c5005c1ceb28dbf646f8a8d2c82f825
Thibaud Lambert <Thibaud.Lambert@gmail.com>
2014-08-23 08:15:13 +0200

    Project initialization: libgdx

---
+++ desktop/src/com/ludum/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ludum.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ludum.game.LudumGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumGame(), config);
+	}
+}
