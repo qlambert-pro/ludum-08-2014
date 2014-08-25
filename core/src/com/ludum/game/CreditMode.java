8aee620ad057c24d0791f8adf73ede576fee15ba
Thibaud Lambert <Thibaud.Lambert@gmail.com>
2014-08-26 00:20:12 +0200

    end and start

--- core/src/com/ludum/game/CreditMode.java
+++ core/src/com/ludum/game/CreditMode.java
@@ -3,16 +3,23 @@ package com.ludum.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ludum.rendering.TextureManager;
 
 public class CreditMode extends ScreenAdapter {
 	private SpriteBatch batch;
-	private float timeCount;	
+	private float timeCount;
+	private OrthographicCamera cam;
+	private Texture end;
 	
 	public CreditMode(LudumGame g) {
 		batch = new SpriteBatch();
 		timeCount = 0; 
+		cam = new OrthographicCamera(1,1);
+		cam.update();
+		end = new Texture(Gdx.files.internal("end.png"));
 	}
 	
 	@Override
@@ -21,8 +28,9 @@ public class CreditMode extends ScreenAdapter {
 		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(cam.combined);
 		batch.begin();
-		batch.draw(TextureManager.getInstance().getEnd(), 0, 0);
+		batch.draw(end,-0.5f,-0.5f,1,1);
 		batch.end();
 
 		if(timeCount>5)
--- core/src/com/ludum/game/StartMode.java
+++ core/src/com/ludum/game/StartMode.java
@@ -1,87 +1,51 @@
 package com.ludum.game;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.ludum.sound.SoundManager;
 
 public class StartMode extends ScreenAdapter {
 	private LudumGame game;
-	private TextButton classicMode;
+	private OrthographicCamera cam;
 	private SpriteBatch batch;
-	private OrthographicCamera camera;
-	private Stage stage;
-	private BitmapFont font;
-	private TextureAtlas buttonsAtlas;
-	private Skin buttonSkin;
+	private Texture start;
+	private InputProcessor control;
 
 	public StartMode(LudumGame g) {
 		game = g;
-
-		camera = new OrthographicCamera(Gdx.graphics.getWidth(),
-				Gdx.graphics.getHeight());
-
 		batch = new SpriteBatch();
-
-		buttonsAtlas = new TextureAtlas(Gdx.files.internal("button.pack"));
-		buttonSkin = new Skin();
-		buttonSkin.addRegions(buttonsAtlas);
-		font = new BitmapFont();
-
-		stage = new Stage();
-
-		TextButtonStyle style = new TextButtonStyle();
-		style.up = buttonSkin.getDrawable("buttonOff");
-		style.over = buttonSkin.getDrawable("buttonOn");
-		style.font = font;
-
-		classicMode = new TextButton("Start", style);
-		classicMode.setPosition(225, 300);
-		classicMode.setHeight(50);
-		classicMode.setWidth(200);
-		classicMode.addListener(new InputListener() {
-			public boolean touchDown(InputEvent event, float x, float y,
-					int pointer, int button) {
-				game.startClassicMode();
-				return true;
+		start = new Texture(Gdx.files.internal("start.png"));
+		cam = new OrthographicCamera(1,1);
+		cam.update();
+		control = new InputAdapter() {
+			@Override
+			public boolean keyDown(int keycode) {
+				switch (keycode) {
+				case Keys.ENTER:
+					game.removeInputProcessor(control);
+					game.startClassicMode();
+					break;
+				}
+				return false;
 			}
-		});
-
-		stage.addActor(classicMode);
-		SoundManager.getInstance();
-	}
-
-	@Override
-	public void show() {
-		game.addInputProcessor(stage);
-	}
-
-	@Override
-	public void hide() {
-		game.removeInputProcessor(stage);
+		};
+		game.addInputProcessor(control);
 	}
 
 	@Override
 	public void render(float dt) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		stage.act();
-
-		batch.setProjectionMatrix(camera.combined);
+		batch.setProjectionMatrix(cam.combined);
 		batch.begin();
-		stage.draw();
+		batch.draw(start,-0.5f,-0.5f,1,1);
 		batch.end();
+
 	}
 }
